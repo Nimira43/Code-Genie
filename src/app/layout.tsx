b41546f62fb8b6cee786f52e46fc16a421a51e8d
Nimira43 <n.rabone72@aol.com>
2024-12-14 23:41:53 +0000

    Brought in the ConvexClientProvider into the layout.jsx and wrapped it around the children

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Poppins } from 'next/font/google' 
 import './globals.css'
 import { ClerkProvider } from '@clerk/nextjs'
+import ConvexClientProvider from '@/components/providers/ConvexClientProvider'
 
 const poppins = Poppins({
   subsets: ['latin'],
@@ -22,10 +23,11 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang='en'>
         <body className={poppins.className}>
-          {children}
+          <ConvexClientProvider>
+            {children}
+          </ConvexClientProvider>
         </body>
       </html>
     </ClerkProvider>
-    
   )
 }
