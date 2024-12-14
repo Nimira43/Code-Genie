24d86d6855f4cb02413895ee0a4f68ccce78fb85
Nimira43 <n.rabone72@aol.com>
2024-12-14 21:39:41 +0000

    Brought in Doto font for logo. Logo styled

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { SignedIn, SignedOut, SignOutButton, SignUpButton } from '@clerk/nextjs'
 export default function page() {
   return (
     <div>
-      <h1>Code Genie</h1>
+      <h1 className='logo'>Code Genie</h1>
       <SignedOut>
         <SignUpButton>
           <button className='bg-[#ff4500] text-[#fffaf0] py-2 px-4 rounded-md hover:bg-[#111]'>
