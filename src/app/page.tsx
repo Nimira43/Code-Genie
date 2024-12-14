f28c630d0c9e2e2a99422ea049e14a3186696bf1
Nimira43 <n.rabone72@aol.com>
2024-12-14 21:26:20 +0000

    Testing Clerking by bringing some sign in/out components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+import { SignedIn, SignedOut, SignOutButton, SignUpButton } from '@clerk/nextjs'
+
 export default function page() {
   return (
     <div>
       <h1>Code Genie</h1>
+      <SignedOut>
+        <SignUpButton />
+      </SignedOut>
+      <SignedIn>
+        <SignOutButton />
+      </SignedIn>
     </div>
   )
 }
