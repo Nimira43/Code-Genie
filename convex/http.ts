4d5ecee48e2b5966b3bbbf933996b7eb175f8d88
Nimira43 <n.rabone72@aol.com>
2024-12-15 14:42:01 +0000

    Checking for svix headers

--- convex/http.ts
+++ convex/http.ts
@@ -6,6 +6,7 @@ const http = httpRouter()
 http.route({
   path: '/clerk-webhook',
   method: 'POST',
+  
   handler: httpAction(async (ctx, request) => {
     const webhookSecret = process.env.CLERK_WEBHOOK_SECRET
     if (!webhookSecret) {
@@ -14,5 +15,11 @@ http.route({
     const svix_id = request.headers.get('svix_id')
     const svix_signature = request.headers.get('svix-signature')
     const svix_timestamp = request.headers.get('svix-timestamp')
+
+    if (!svix_id || !svix_signature || !svix_timestamp) {
+      return new Response('Error occurred -- no svik header', {
+        status: 400,
+      })
+    }
   })
 })
